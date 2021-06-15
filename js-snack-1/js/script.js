dba344f348448f8ef544b80c20aeb3647fd1e67f
cromeo98 <romeo.christian98@gmail.com>
2021-06-15 15:06:26 +0200

    add: less weight bike formula

--- js-snack-1/js/script.js
+++ js-snack-1/js/script.js
@@ -1,2 +1,33 @@
 // 1. Creare un array di oggetti. Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 // Stampare a schermo la bici con peso minore.
+
+//1) creo l'array di oggetti
+var bikes = [
+    {
+        name: 'mountain bike',
+        weigth: 20
+    },
+    {
+        name: 'fast bike',
+        weigth: 10
+    },
+    {
+        name: 'graziella',
+        weigth: 15
+    }
+];
+
+//2) attribuisco un valore peso minimo alla bici n° 1[0] dell'array
+var weigthMin = bikes[0].weigth;
+
+console.log(weigthMin);
+
+//3) utilizzo un ciclo for (che parte dal secondo oggetto [1] perchè il primo è quello che abbiamo attribuito alla variabile) per sostituire il peso minimo ogni volta che trova un oggetto di peso inferiore
+for (var i = 1; i < bikes.length; i++){
+    if(bikes[i].weigth < weigthMin){
+        weigthMin = bikes[i].weigth
+    }
+}
+
+console.log(weigthMin);
+
